9526f9085db6da77476ca2320c41c9f98043b899
Anatoliy Morozov <anatoly.morozov@radiogigabit.net>
2025-05-23 00:28:42 +0300

    Added Bin, view, changes in form

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Morozov_tomogram_visualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            открытьToolStripMenuItem = new ToolStripMenuItem();
+            glControl1 = new OpenTK.GLControl.GLControl();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { открытьToolStripMenuItem });
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(48, 20);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            открытьToolStripMenuItem.Size = new Size(180, 22);
+            открытьToolStripMenuItem.Text = "Открыть";
+            открытьToolStripMenuItem.Click += открытьToolStripMenuItem_Click;
+            // 
+            // glControl1
+            // 
+            glControl1.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
+            glControl1.APIVersion = new Version(3, 3, 0, 0);
+            glControl1.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
+            glControl1.IsEventDriven = true;
+            glControl1.Location = new Point(26, 80);
+            glControl1.Name = "glControl1";
+            glControl1.Profile = OpenTK.Windowing.Common.ContextProfile.Core;
+            glControl1.SharedContext = null;
+            glControl1.Size = new Size(469, 258);
+            glControl1.TabIndex = 1;
+            glControl1.Paint += glControl1_Paint;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(glControl1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem открытьToolStripMenuItem;
+        private OpenTK.GLControl.GLControl glControl1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Morozov_tomogram_visualizer
+{
+    public partial class Form1 : Form
+    {
+        Bin bin = new Bin();
+        View view = new View();
+        bool loaded = false;
+        int currentLayer = 0;
+        bool needReload = false;
+        int FrameCount = 0;
+        DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void îòêðûòüToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string str = dialog.FileName;
+                bin.readBIN(str);
+                view.SetupView(glControl1.Width, glControl1.Height);
+                loaded = true;
+                glControl1.Invalidate();
+            }
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (loaded)
+            {
+                view.DrawQuads(currentLayer);
+                glControl1.SwapBuffers();
+            }
+        }
+    }
+}
